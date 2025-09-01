b0e399443d8be3a301657575803ac0d091d3730e
sathishkumar7200 <kkmmurugesansathish@gmail.com>
2025-09-01 13:40:39 +0530

    Success

---
+++ exp.py
@@ -0,0 +1,15 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("Get request received...")
+        self.send_response(200) 
+        self.send_header("content-type", "1/html")       
+        self.end_headers()
+        self.wfile.write(content.encode())
+
+print("This is my webserver") 
+server_address =('',8000)
+httpd = HTTPServer(server_address,MyServer)
